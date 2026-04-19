465232488447155b973a5a0223726dddff114fb0
ShokoriS <the4thgenius@gmail.com>
2026-04-19 12:11:25 +0430

    Refactor alternating_symbols function logic

--- exer2.py
+++ exer2.py
@@ -9,13 +9,12 @@ here the symbols are space and the hash I will allow it so it could be anything.
 
 #to solve this problem I will try to use a switch technique to give it the same shape
 
-def alternating_symbols(height, symbol='#'):
+def alternating_symbols(height):
     # I have used a tuple to indicated as to should I add a space or not...(really avoiding an if here)
-    switch_symbols = ('', ' ')
+    switch_symbols = ('a', 'b')
     # writing the each_line for code readability... 
-    each_line = symbol * height
     
-    shape = "\n".join(f"{switch_symbols[i % 2]}{each_line}" for i in range(height))
+    shape = "\n".join("".join(f'{switch_symbols[j % 2]}' for j in range(i, height + i)) for i in range(height))
     return shape
 
-print(alternating_symbols(5))
+print(alternating_symbols(9))
