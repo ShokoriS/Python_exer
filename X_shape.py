9e68f2a32d78fc129c7419daabd3ca6db1bb84ff
ShokoriS <the4thgenius@gmail.com>
2026-04-27 10:24:14 +0430

    Refactor X_shape function and update calls
    Refactor X_shape function into X_shape2 for improved structure and readability. Update function calls to use height 7 instead of 19 and 10.

--- X_shape.py
+++ X_shape.py
@@ -14,25 +14,44 @@ def X_shape(height):
         f"{'*' * (j - 1)}{height - j + 1}{'*' * (height - j * 2 + (not remainder))}{height - j + 1}" for j in range(half_height, 0, -1)
         ))
 
-X_shape(19)
+X_shape(7)
+
+
+def X_shape2(height):
+    
+    remainder = height % 2
+    half_height = height // 2
+    extra_spaces =1 if remainder == 0 else 0 
+    get_len_number = lambda number: len(str(number)) -1
+    # Top half
+    for i in range(1, 1 + half_height + remainder):
+        
+        left_spaces = ' ' * (i - 1 + extra_spaces)
+        
+        middle_spaces = ' ' * (height - i * 2 - (not remainder) - get_len_number(i))
+        
+        right_number = '' if i == (half_height + remainder) else i
+        
+        line = f"{left_spaces}{i}{middle_spaces}{right_number}"
+        
+        print(line)
+
+    # Bottom half
+    for j in range(half_height, 0, -1):
+         
+        left_spaces = ' ' * (j - 1)
+        
+        number = height - j + 1
+        
+
+        middle_spaces = ' ' * (height - j * 2 + (not remainder) - get_len_number(number))
+        
+        line = f"{left_spaces}{number}{middle_spaces}{number}"
+        
+        print(line)
+
+
+X_shape2(7)
 
 
 
-print(int(True))
-
-
-
-def X_shape(height):
-    width = len(str(height))
-
-    print("\n".join(
-        "".join(
-            f"{i:>{width}}" if j == i else
-            f"{height - i + 1:>{width}}" if j == height - i + 1 else
-            " " * width
-            for j in range(1, height + 1)
-        )
-        for i in range(1, height + 1)
-    ))
-
-X_shape(10)
